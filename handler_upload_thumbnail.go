96ab2faa178758a7c55c2e7c48a058aa4d24a899
fenroe <scrambles9494@gmail.com>
2025-01-20 13:42:06 +0200

    storing files on disk

--- handler_upload_thumbnail.go
+++ handler_upload_thumbnail.go
@@ -1,10 +1,16 @@
 package main
 
 import (
+	"crypto/rand"
 	"encoding/base64"
+	"errors"
 	"fmt"
 	"io"
+	"mime"
 	"net/http"
+	"os"
+	"path/filepath"
+	"strings"
 
 	"github.com/bootdotdev/learn-file-storage-s3-golang-starter/internal/auth"
 	"github.com/google/uuid"
@@ -39,14 +45,34 @@ func (cfg *apiConfig) handlerUploadThumbnail(w http.ResponseWriter, r *http.Requ
 		respondWithError(w, http.StatusBadRequest, "Couldn't verify thumbnail", err)
 		return
 	}
-	fileType := fileHeader.Header.Get("Content-Type")
-	imageData, err := io.ReadAll(file)
+	mediaType, _, err := mime.ParseMediaType(fileHeader.Header.Get("Content-Type"))
 	if err != nil {
-		respondWithError(w, http.StatusInternalServerError, "Couldn't read image data", err)
+		respondWithError(w, http.StatusInternalServerError, "Couldn't parse filetype", err)
 		return
 	}
-	encodedImage := base64.StdEncoding.EncodeToString(imageData)
-	dataURL := fmt.Sprintf("data:%s;base64,%s", fileType, encodedImage)
+	if mediaType != "image/jpeg" && mediaType != "image/png" {
+		respondWithError(w, http.StatusBadRequest, "Invalid file type", errors.New("file must be either png or jpeg format"))
+		return
+	}
+	fileType := strings.Split(fileHeader.Header.Get("Content-Type"), "/")[1]
+	fileNameBits := make([]byte, 32)
+	_, err = rand.Read(fileNameBits)
+	if err != nil {
+		respondWithError(w, http.StatusInternalServerError, "Couldn't create filename", err)
+		return
+	}
+	fileName := base64.RawURLEncoding.EncodeToString(fileNameBits)
+	fileURL := fmt.Sprintf("%s.%s", filepath.Join(cfg.assetsRoot, fileName), fileType)
+	imageFile, err := os.Create(fileURL)
+	if err != nil {
+		respondWithError(w, http.StatusInternalServerError, "Couldn't create image file", err)
+		return
+	}
+	_, err = io.Copy(imageFile, file)
+	if err != nil {
+		respondWithError(w, http.StatusInternalServerError, "Couldn't write to image file", err)
+		return
+	}
 	video, err := cfg.db.GetVideo(videoID)
 	if err != nil {
 		respondWithError(w, http.StatusInternalServerError, "Couldn't retrieve video metadata", err)
@@ -56,7 +82,8 @@ func (cfg *apiConfig) handlerUploadThumbnail(w http.ResponseWriter, r *http.Requ
 		respondWithError(w, http.StatusUnauthorized, "Video does not belong to authenticated user", err)
 		return
 	}
-	video.ThumbnailURL = &dataURL
+	thumbnailURL := fmt.Sprintf("http://localhost:%v/%s", cfg.port, fileURL)
+	video.ThumbnailURL = &thumbnailURL
 	err = cfg.db.UpdateVideo(video)
 	if err != nil {
 		respondWithError(w, http.StatusInternalServerError, "Could not update video with thumbnail url", err)
